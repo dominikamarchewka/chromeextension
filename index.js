b2a984e50109a10fcf87b251ea7eb51da7710a35
dominikamarchewka <dominika_marchewka@wp.pl>
2021-08-02 15:00:13 +0200

    Getting current tab from chrome

--- index.js
+++ index.js
@@ -1,39 +1,46 @@
-
 const inputBtn = document.getElementById("input-btn");
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-
-deleteBtn.addEventListener("dblclick", function(){
-    localStorage.clear();
-    myLeads = [];
-    renderLeads();
-});
+const tabBtn = document.getElementById("tab-btn");
 
 if (leadsFromLocalStorage){
     myLeads = leadsFromLocalStorage
-    renderLeads()
+    render(myLeads)
+}
+
+function render(leads){
+    let listItems = "";
+    for(let i=0; i < leads.length; i++){
+        listItems +=
+            `<li> 
+        <a target='_blank' href='${leads[i]}'> 
+            ${leads[i]} 
+        </a>
+        </li>`
+    }
+    ulEl.innerHTML = listItems
 }
 
 inputBtn.addEventListener("click", function (){
     myLeads.push(inputEl.value)
     inputEl.value=null
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    renderLeads()
-});
+    render(myLeads)
+})
 
+deleteBtn.addEventListener("dblclick", function(){
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
 
-function renderLeads(){
-    let listItems = "";
-    for(let i=0; i < myLeads.length; i++){
-        listItems +=
-            `<li> 
-    <a target='_blank' href='${myLeads[i]}'> 
-        ${myLeads[i]} 
-    </a>
-    </li>`
-    }
-    ulEl.innerHTML = listItems
-}
+tabBtn.addEventListener("click", function(){
+    chrome.tabs.query({active:true, currentWindow:true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
+})
--- temp.js
+++
@@ -1,5 +0,0 @@
-myLeads = '["www.facebook.pl"]'
-myLeads = JSON.parse(myLeads);
-myLeads.push("www.domi.pl");
-myLeads = JSON.stringify(myLeads);
-console.log(typeof myLeads)
